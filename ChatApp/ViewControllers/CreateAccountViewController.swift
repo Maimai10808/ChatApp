4ff913a4f196284df7f026161f53b1cc79d3851a
Maimai10808 <331686549@qq.com>
2025-07-18 20:27:46 +0800

    5.KeyboardAvoidingScroll
    Making Sure SignIn TextFields are Visible When Keyboard Shows

--- ChatApp/ViewControllers/CreateAccountViewController.swift
+++ ChatApp/ViewControllers/CreateAccountViewController.swift
@@ -16,9 +16,22 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    var activeTextField: UITextField?
+    
+    override func viewWillAppear(_ animated: Bool) {
+                  super.viewWillAppear(animated)
+                   registerKeyboardNotifications()
+       }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameTextField.delegate = self
+        emailTextField   .delegate = self
+        passwordTextField.delegate = self
+        
         containerView.clipsToBounds = true
         containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         
@@ -37,12 +50,69 @@ class CreateAccountViewController: UIViewController {
         signinAccountTextView.isScrollEnabled    = false
         signinAccountTextView.textAlignment      = .center
         signinAccountTextView.isEditable         = false
+        
+        tapRecognizerDismissKeyBoard()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         containerView.layer.cornerRadius = 20
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+                 super.viewWillDisappear(animated)
+                   removeKeyboardNotifications()
+       }
+    
+    func registerKeyboardNotifications() {
+            NotificationCenter.default.addObserver(self,
+                selector: #selector(keyboardWillShow),
+                name    : UIResponder.keyboardWillShowNotification,
+                object  : nil)
+
+            NotificationCenter.default.addObserver(self,
+                selector: #selector(keyboardWillHide),
+                name    : UIResponder.keyboardWillHideNotification,
+                object  : nil)
+        }
+        
+        func removeKeyboardNotifications() {
+            NotificationCenter.default.removeObserver(self,
+                                                      name  : UIResponder.keyboardWillShowNotification ,
+                                                      object: nil)
+            NotificationCenter.default.removeObserver(self,
+                                                      name  : UIResponder.keyboardWillHideNotification ,
+                                                      object: nil)
+        }
+        
+        @objc func keyboardWillShow(_ notification: Notification) {
+            
+            guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+
+            let totalOffset = keyboardHeight + (activeTextField?.frame.height ?? 0)
+
+            scrollView.contentInset.bottom = totalOffset
+        }
+        
+        
+        
+        @objc func keyboardWillHide(_ notification: Notification) {
+            scrollView.contentInset.bottom = 0
+        }
+        
+        func tapRecognizerDismissKeyBoard() {
+            let tap = UITapGestureRecognizer(target: self,
+                                             action: #selector(dismissKeyboard))
+            view.addGestureRecognizer(tap)
+
+        }
+        
+        @objc func dismissKeyboard() {
+            view.endEditing(true)
+        }
+
     
     @IBAction func createAccountButtonTapped(_ sender: Any) {
     }
@@ -60,3 +130,17 @@ extension CreateAccountViewController: UITextViewDelegate {
         return false
     }
 }
+
+extension CreateAccountViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+    }
+    
+    func textFieldDidEndEditing  (_ textField: UITextField) {
+        activeTextField = nil
+    }
+    
+}
+
+
--- ChatApp/ViewControllers/SignInViewController.swift
+++ ChatApp/ViewControllers/SignInViewController.swift
@@ -15,12 +15,92 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
+    var activeTextField: UITextField?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTextField   .delegate = self
+        passwordTextField.delegate = self
+        
+        drawcontainerView()
+        drawcreateAccountTextView()
+        
+        tapRecognizerDismissKeyBoard()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+                  super.viewWillAppear(animated)
+                   registerKeyboardNotifications()
+       }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        containerView.layer.cornerRadius = 20
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+                 super.viewWillDisappear(animated)
+                   removeKeyboardNotifications()
+       }
+    
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name    : UIResponder.keyboardWillShowNotification,
+                                               object  : nil)
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name    : UIResponder.keyboardWillHideNotification,
+                                               object  : nil)
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self,
+                                                  name  : UIResponder.keyboardWillShowNotification ,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name  : UIResponder.keyboardWillHideNotification ,
+                                                  object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+
+        let totalOffset = keyboardHeight + (activeTextField?.frame.height ?? 0)
+
+        scrollView.contentInset.bottom = totalOffset
+    }
+    
+    
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        scrollView.contentInset.bottom = 0
+    }
+    
+    func tapRecognizerDismissKeyBoard() {
+        let tap = UITapGestureRecognizer(target: self,
+                                         action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+
+    }
+    
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func drawcontainerView() {
         containerView.clipsToBounds       = true
         containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
+    }
+    
+    func drawcreateAccountTextView() {
         let attributedString = NSMutableAttributedString(
             string    : "Don't have an account? Create an acount here",
             attributes: [NSAttributedString.Key.font : Font.linkLabel])
@@ -38,10 +118,7 @@ class SignInViewController: UIViewController {
         createAccountTextView.isEditable         = false
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        containerView.layer.cornerRadius = 20
-    }
+    
 
     @IBAction func signinButtonTapped(_ sender: Any) {
         
@@ -60,4 +137,16 @@ extension SignInViewController: UITextViewDelegate {
         
         return false
     }
+}
+
+extension SignInViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+    }
+    
+    func textFieldDidEndEditing  (_ textField: UITextField) {
+        activeTextField = nil
+    }
+    
 }
