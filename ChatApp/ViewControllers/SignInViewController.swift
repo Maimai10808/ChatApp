e1754588782bf3348156de74d07ad5078d61e656
Maimai10808 <331686549@qq.com>
2025-07-19 18:36:39 +0800

    14. Sign In

--- ChatApp/ViewControllers/SignInViewController.swift
+++ ChatApp/ViewControllers/SignInViewController.swift
@@ -6,10 +6,11 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
     
-    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentView  : UIView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var createAccountTextView: UITextView!
     @IBOutlet weak var emailTextField: UITextField!
@@ -123,8 +124,60 @@ class SignInViewController: UIViewController {
 
     @IBAction func signinButtonTapped(_ sender: Any) {
         
+        guard let password = passwordTextField.text else {
+            presentErrorAlert(title  : "Password Required",
+                              message: "Please enter a Password to continue.")
+            return
+        }
+        
+        guard let email = emailTextField.text else {
+            presentErrorAlert(title  : "Email Required",
+                              message: "Please enter a Email to continue.")
+            return
+        }
+        
+        showLoadingView()
+        Auth.auth().signIn(withEmail: email, password: password) { _, error in
+            
+            self.removeLoadingView()
+            
+            if let error = error {
+                print(error.localizedDescription)
+                
+                var errorMessage = "Something went wrong. Please try again later."
+                
+                let errorCode = (error as NSError).code
+                
+                if let authError = AuthErrorCode(rawValue: errorCode) {
+                    switch authError {
+                    case .invalidEmail:
+                        errorMessage = "Invalid email."
+                    case .userNotFound:
+                        errorMessage = "Email/password does not match any record."
+                    default:
+                        break
+                    }
+                }
+                
+                self.presentErrorAlert(title  : "Sign In Failed",
+                                       message: errorMessage )
+                
+                return
+            }
+            
+            self.view.endEditing(true) // Dismiss keyboard safely before transition
+            
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let homeVC = mainStoryboard.instantiateViewController(withIdentifier: "HomeViewController")
+            let navVC = UINavigationController(rootViewController: homeVC)
+            
+            let window = UIApplication.shared.connectedScenes
+                .flatMap{ ($0 as? UIWindowScene)?.windows ?? [] }
+                .first { $0.isKeyWindow }
+            
+            window?.rootViewController = navVC
+        }
     }
-
 }
 
 
